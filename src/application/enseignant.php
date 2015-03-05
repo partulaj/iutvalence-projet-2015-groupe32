f477db56decbf64ea8c6ec9e6117f859cc1d2fd0
ouladnam <ouladnam@PC-B103-07.iut-valence.fr>
2015-03-05 15:19:35 +0100

    Juste pour tester des bouttons

--- src/application/enseignant.php
+++ src/application/enseignant.php
@@ -116,6 +116,7 @@ if (isset($_POST['new_projet'])) {
 								<tr>
 									<th>Numéro de Projet</th>
 									<th>Nom du Projet</th>
+									<th><button type="submit" name="editer" class="icon-edit-floppy"><button type="submit" name="tableau" class="icon-calendar-floppy"><button type="submit" name="mail" class="icon-message-new"></th>
 								</tr>
 								<?php
 								$_SESSION['ens']->afficheMesProjets();
